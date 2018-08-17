fb17f9e3dc7a359fc6515d71ec25550e5100ae93
Middekurva <srinivas.middekurva@capgemini.com>
2018-08-17 08:53:32 -0400

    First commit

---
+++ amstrong.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class amstrong {
+    int ams(int n)
+    {
+    	int x=0;
+		while(n>0)
+		{
+			int i=n%10;
+						
+			x=x+(i*i*i);
+			n=n/10;
+
+			
+		}
+		return x;
+	}
+	public static void main(String[] args){
+		int num;
+		System.out.print("Enter a number");
+		Scanner v = new Scanner(System.in);
+		num = v.nextInt();
+		amstrong a=new amstrong();
+		int val = a.ams(num);
+		if(val!=num)
+			System.out.print("Given number is not an amstrong.");
+		else
+			System.out.print("Given number is an amstrong.");
+	}
+
+}
